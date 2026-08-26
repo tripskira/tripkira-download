221c787fa9a8265164a1a918da183b6ad751b6ef
tripskira <tripskira@gmail.com>
2026-08-27 07:04:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.TRIPKIRA_CONFIG={
   version:"v3.32",
   apkFileName:"TripKIRA-v3.32.apk",
-  apkUrl:"https://github.com/tripskira/tripkira-download/releases/download/v3.32/TripLogger_v3_3_2.apk"
+  apkUrl:"https://github.com/tripskira/tripkira-download/releases/download/v3.32/TripLogger_v3_3_2_latest.apk"
 };
